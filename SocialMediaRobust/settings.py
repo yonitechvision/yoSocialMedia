f61fe885dc6941092cd091fcf5b02b651bd933cc
yonitechvision <desisayonatan77@gmail.com>
2024-10-07 16:00:43 +0300

    including allowed hosts

--- SocialMediaRobust/settings.py
+++ SocialMediaRobust/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)%1gp0cp)x-p&e1*6fht6t^ws@dwgmz(@*b@&+)=mmv#efg7kv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yobetty.pythonanywhere.com']
 
 
 # Application definition
