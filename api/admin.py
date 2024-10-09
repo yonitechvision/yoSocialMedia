bc0b55e85d2e2075056d74e02d38eca7cdf23e35
yonitechvision <desisayonatan77@gmail.com>
2024-10-09 16:12:52 +0300

    register models in admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Profile, Post, Like, Comment, Follower, Notification, Message, Repost, Hashtag, PostHashtag
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(Follower)
+admin.site.register(Notification)
+admin.site.register(Message)
+admin.site.register(Repost)
+admin.site.register(Hashtag)
+admin.site.register(PostHashtag)
